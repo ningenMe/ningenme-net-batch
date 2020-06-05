5cbd0015e10cf79d6fd712346520f485bd1b7d87
ningenMe <amohkuru@gmail.com>
2020-06-06 02:19:34 +0900

    add logcode

---
+++ src/main/java/ningenme/net/batch/util/LogCode.java
@@ -0,0 +1,12 @@
+package ningenme.net.batch.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LogCode {
+    public static final Map<String,String> map = new HashMap<String, String>() {
+        {
+            put("e001", "e001: スクレイピングの失敗");
+        }
+    };
+}
