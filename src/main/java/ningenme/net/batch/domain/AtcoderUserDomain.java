65df777fb1783efc7dfd2f42701eb71a463bcb9b
ningenMe <amohkuru@gmail.com>
2020-06-06 02:18:53 +0900

    add scrape in atcoder_user_domain

--- src/main/java/ningenme/net/batch/domain/AtcoderUserDomain.java
+++ src/main/java/ningenme/net/batch/domain/AtcoderUserDomain.java
@@ -2,6 +2,12 @@ package ningenme.net.batch.domain;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import ningenme.net.batch.BatchApplication;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
@@ -21,6 +27,11 @@ public class AtcoderUserDomain implements Serializable {
     private Integer ratedMatches;
     private Timestamp createdTime;
     private Timestamp updatedTime;
+
+    //for scraping
+    private final String atcoderUserPageUrlPrefix = "https://atcoder.jp/users/";
+    private Document document;
+    private Elements element;
 
     public AtcoderUserDomain() {
         this.atcoderId = "";
@@ -46,12 +57,23 @@ public class AtcoderUserDomain implements Serializable {
     public void setCodeforcesId(String codeforcesId) {
         this.codeforcesId = codeforcesId;
     }
-
     public Integer getRank() {
         return this.rank;
     }
     public void setRank(Integer rank) {
         this.rank = rank;
+    }
+    public Integer getCurretntRate() {
+        return this.currentRate;
+    }
+    public void setCurrentRate(Integer currentRate) {
+        this.currentRate = currentRate;
+    }
+    public Integer getHighestRate() {
+        return this.highestRate;
+    }
+    public void setHighestRate(Integer highestRate) {
+        this.highestRate = highestRate;
     }
     public Timestamp getCreatedTime() {
         return this.createdTime;
@@ -65,12 +87,53 @@ public class AtcoderUserDomain implements Serializable {
     public void setUpdatedTime(Timestamp updatedTime) {
         this.updatedTime = updatedTime;
     }
+    public void setAtcoderUserDomainFromAtcoderPage() throws Exception {
+        for(int i = 0; i < 2; i++) {
+            try{
+                this.document = Jsoup.connect(this.atcoderUserPageUrlPrefix+this.atcoderId).get();
+                this.element = document.select("table tr");
+                setTableElementToDomain();
+                Thread.sleep(3000L);
+            }
+            catch(Exception e) {
+                BatchApplication.logger.error(e.toString());
+                continue;
+            }
+            return;
+        }
+        throw new Exception("e001");
+    }
+    private void setTableElementToDomain(){
+        for(int i = 0; i < element.size(); i++) {
+            String key = element.get(i).select("th").text();
+            String value = element.get(i).select("td").text();
+            System.out.println(key+" : "+value);
+            switch(key) {
+                case "TopCoder ID":
+                    setTopcoderId(value);
+                    break;
+                case "Codeforces ID":
+                    setCodeforcesId(value);
+                    break;
+                case "Rank":
+                    setRank( Integer.valueOf(value.substring(0, value.length()-2)) );
+                    break;
+                case "Rating":
+                    setCurrentRate(Integer.valueOf(value));
+                    break;
+                case "Highest Rating":
+                    setHighestRate(Integer.valueOf(value.split(" ")[0]));
+                    break;
+            }
+        }
+    }
     @Override
     public String toString(){
         String ret = "{";
         ret += "atcoderId: "    + this.atcoderId    + ",\n";
         ret += "topcoderId: "   + this.topcoderId   + ",\n";
         ret += "codeforcesId: " + this.codeforcesId + ",\n";
+        ret += "rank: "         + this.rank         + ",\n";
         ret += "}";
         return ret;
     }
