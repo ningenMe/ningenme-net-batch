279bf712c717336d9d60314c55684059022d4240
ningenMe <amohkuru@gmail.com>
2020-06-06 02:18:21 +0900

    add logger on global

--- src/main/java/ningenme/net/batch/BatchApplication.java
+++ src/main/java/ningenme/net/batch/BatchApplication.java
@@ -1,12 +1,17 @@
 package ningenme.net.batch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
 @SpringBootApplication
 @EnableScheduling
 public class BatchApplication {
+
+	public static final Logger logger = LoggerFactory.getLogger(BatchApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BatchApplication.class, args);
 	}
