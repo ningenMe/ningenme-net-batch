cf6a86e05b7072b2d053e0b2739198499c8dd032
ningenMe <amohkuru@gmail.com>
2020-06-11 00:19:14 +0900

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+./mvnw clean
+./mvnw package
+for key in SLACK_WEBHOOK_URL MYSQL_HOST MYSQL_PORT MYSQL_DB MYSQL_USER MYSQL_PASSWORD MYSQL_MYBATIS_URL
+do
+value=`sudo -u ec2-user aws ssm get-parameters --name $key --query "Parameters[*].{Value: Value}" --output text`
+export $key=$value
+done
+pid=`jps | awk -F: '/ningenme-net-batch.jar/{print $1}' | awk '{print $1}'`
+if [ $pid != "" ] ; then
+    kill -9 $pid
+fi
+java -jar target/ningenme-net-batch.jar &
+curl -X POST --data-urlencode "payload={\"text\":\"succeeded api-java deploy\",\"channel\":\"#log-info\",}" $SLACK_WEBHOOK_URL
